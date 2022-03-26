116fed04ac8f048852b14d1a45c3c5405c6635ad
Amani Louendriz <aloue024@uottawa.ca>
2022-03-26 17:47:07 -0400

    Pushed the project to GitHub

---
+++ Utils.java
@@ -0,0 +1,192 @@
+import java.util.Random;
+import java.io.Console;
+
+public class Utils {
+	public static final Random generator = new Random();
+	public static final Console console = System.console();
+	public static final String NEW_LINE = System.getProperty("line.separator");
+
+
+    /**
+     * This method rotates array of size lines * columns referenced
+     * by transformedBoard by 90 degree clockwise. For example, the
+     * 3*3 game
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method rotates it into the game
+     *
+     * 7 | 4 | 1
+     * ----------
+     * 8 | 5 | 2
+     * ----------
+     * 9 | 6 | 3
+     *
+     * that is, after the method, transformedBoard = {7,4,1,8,5,2,9,6,3}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+
+    public static void rotate(int lines, int columns, int[] transformedBoard){
+        // YOUR CODE HERE
+        int indice = 0;
+        int [] rotaRes = new int [lines*columns];
+        for (int i = 0; i < lines; i++){
+            indice = i * lines;
+            for (int j = 0; j < columns; j++){
+                rotaRes[indice + j] = transformedBoard[(lines -1 -j)*lines + indice - (lines - 1)*i];
+            }
+        }
+        System.arraycopy(rotaRes,0,transformedBoard,0,lines*columns);
+        transformedBoard = rotaRes;
+    }
+
+    /**
+     * This method does an horizontal symmetry on array of size lines * columns referenced
+     * by transformedBoard. For example, the
+     * 3*3 game
+     *
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method transforms it into the game   
+     *
+     * 7 | 8 | 9
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 1 | 2 | 3
+     *
+     * that is, after the method, transformedBoard = {7,8,9,4,5,6,1,2,3}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+    public static  void horizontalFlip(int lines, int columns, int[] transformedBoard){
+
+        // YOUR CODE HERE  
+        int indice = 0;
+        for(int i=0;i<lines/2;i++) {
+            indice = columns * i;//marque le debut de chaque ligne
+            int indiceC = columns*(lines-1-i);// cette variable marque le debut de chaque 
+            //ligne oppose commencant du bas et puis ca remonte jusqu'a ce que tout soit change
+            for(int j=0;j<columns;j++) {//j se deplace dans la ligne et est initialise a 0 a cha
+            //fois qu'on change la ligne
+               int ech = transformedBoard[indice+j];
+               transformedBoard[indice+j] = transformedBoard[indiceC+j];
+               transformedBoard[indiceC+j] = ech;
+            }
+        }    
+    }
+
+   /**
+     * This method does an vertical symmetry on array of size lines * columns referenced
+     * by transformedBoard. For example, the
+     * 3*3 game
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method transforms it into the game   
+     *
+     * 3 | 2 | 1
+     * ----------
+     * 6 | 5 | 4
+     * ----------
+     * 9 | 8 | 7
+     *
+     * that is, after the method, transformedBoard = {3,2,1,6,5,4,9,8,7}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+    public static  void verticalFlip(int lines, int columns, int[] transformedBoard){
+ 
+        // YOUR CODE HERE
+        for(int i=0;i<columns/2;i++) {
+            int indiceC = columns-i-1;
+            int j = 0;
+            while (j < lines*columns){
+              int ech = transformedBoard[i+j];
+              transformedBoard[i+j] = transformedBoard[indiceC+j];
+              transformedBoard[indiceC+j] = ech;
+              j = j + columns;
+            }
+        }
+
+
+    }
+
+    private static void test(int lines, int columns){
+    	int[] test;
+    	test = new int[lines*columns];
+    	for(int i = 0 ; i < test.length; i++){
+    		test[i] = i;
+    	}
+    	System.out.println("testing " + lines + " lines and " + columns + " columns.");
+    	System.out.println(java.util.Arrays.toString(test));
+    	horizontalFlip(lines,columns,test);
+    	System.out.println("HF => " + java.util.Arrays.toString(test));
+    	horizontalFlip(lines,columns,test);
+    	System.out.println("HF => " + java.util.Arrays.toString(test));
+    	verticalFlip(lines,columns,test);
+    	System.out.println("VF => " + java.util.Arrays.toString(test));
+    	verticalFlip(lines,columns,test);
+    	System.out.println("VF => " + java.util.Arrays.toString(test));
+    	if(lines == columns){
+    		for(int i = 0; i < 4; i++) {
+		    	rotate(lines,columns,test);
+		    	System.out.println("ROT => " + java.util.Arrays.toString(test));    			
+    		}
+    	}
+    }
+
+    public static void main(String[] args){
+    	int[] test;
+    	int lines, columns;
+
+    	test(2,2);
+    	test(2,3);
+    	test(3,3);
+    	test(4,3);
+    	test(4,4);
+
+
+    }
+}
